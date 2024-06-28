b7f948e940ab505f2bd7c460f9745545116314f4
WhizIkem <41946520+WhizIkem@users.noreply.github.com>
2024-06-28 23:08:26 +0100

    feat: Added Header component with navigation and auth buttons, and HeroSection component
    - Implemented Header component with logo, navigation links, and auth buttons for login and create account.
    - Created HeroSection component with introductory text, action buttons, and testimonial section.

--- jamort/src/components/Header/Header.js
+++ jamort/src/components/Header/Header.js
@@ -1,18 +1,27 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import './Header.css';
+
+import logo from '../../assets/images/jamorT-logo.png';
 
 const Header = () => {
-  return (
-    <header>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/sign-in">Sign In</Link></li>
-          <li><Link to="/sign-up">Sign Up</Link></li>
-        </ul>
-      </nav>
-    </header>
-  );
-};
+    return (
+        <header className='header'>
+          <div className='logo'>
+            <img src={logo} alt="Jamor Technology Logo" className='logo-image'></img>
+          </div>
+          <nav className='nav-links'>
+            <a href='#home'>Home</a>
+            <a href='#about-us'>About Us</a>
+            <a href='#services'>Services</a>
+            <a href='#tech-graduates'>Tech Graduates</a>
+            <a href='#companies'>Companies</a>
+          </nav>
+          <div className='auth-buttons'>
+            <button className='login-button'>Login</button>
+            <button className='signup-button'>Create Account</button>
+          </div>
+        </header>
+    );
+}
 
 export default Header;
