76a0d07c865a6689ee33d738d8ff088bb63fea0e
WhizIkem <41946520+WhizIkem@users.noreply.github.com>
2024-07-04 14:09:10 +0100

    styled hero section

--- jamort/src/components/HeroSection/HeroSection.js
+++ jamort/src/components/HeroSection/HeroSection.js
@@ -32,22 +32,24 @@ const HeroSection = () => {
               Company <img src={arrowIcon} alt="arrow" />
             </button>
           </div>
-          <div className="images-testimony">
-            <div className="ellipses">
-              {ellipses.map((src, index) => (
-                <img
-                  key={index}
-                  src={src}
-                  alt={`Ellipse ${index + 1}`}
-                  className="ellipse"
-                />
-              ))}
+          <div className="card">
+            <div className="images-testimony">
+              <div className="ellipses">
+                {ellipses.map((src, index) => (
+                  <img
+                    key={index}
+                    src={src}
+                    alt={`Ellipse ${index + 1}`}
+                    className="ellipse"
+                  />
+                ))}
+              </div>
               <span className="testimony">Over 110k+ professionals trained.</span>
             </div>
+            <a href="#learn-more" className="learn-more">
+              Learn More <img src={arrowIcon} alt="arrow" />
+            </a>
           </div>
-          <a href="#learn-more" className="learn-more">
-            Learn More <img src={arrowIcon} alt="arrow" />
-          </a>
         </div>
         <div className="right-content">
           <ImageBanner />
